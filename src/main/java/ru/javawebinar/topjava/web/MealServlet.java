15d29237aa92fc497c7cd2ae019c64bf336f12e0
Maxim Dymnov <dymnov.dm@gmail.com>
2017-03-27 18:50:16 -0400

    Sorting by date added for mealsWithExceedList

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -57,6 +58,7 @@ public class MealServlet extends HttpServlet {
         List<Meal> mealList = mealService.getList();
         List<MealWithExceed> mealWithExceedList =
                 MealsUtil.getFilteredWithExceeded(mealList, LocalTime.MIN, LocalTime.MAX, 2000);
+        mealWithExceedList.sort(Comparator.comparing(MealWithExceed::getDateTime));
         req.setAttribute("meals", mealWithExceedList);
         req.getRequestDispatcher(forward).forward(req, resp);
     }
