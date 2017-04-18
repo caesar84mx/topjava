b156eea6fa67558b9a9a2cc17419487b190a3391
Maxim Dymnov <dymnov.dm@gmail.com>
2017-04-18 12:10:58 -0400

    Save method fix

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -4,6 +4,7 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.persistence.EntityManager;
@@ -21,15 +22,14 @@ public class JpaMealRepositoryImpl implements MealRepository {
     @Override
     @Transactional
     public Meal save(Meal meal, int userId) {
-        if (meal.getUser().getId() == userId) {
-            if (meal.isNew()) {
+        User ref = em.getReference(User.class, userId);
+        meal.setUser(ref);
+
+        if (meal.isNew()) {
                 em.persist(meal);
                 return meal;
             }
             else return em.merge(meal);
-        }
-
-        return null;
     }
 
     @Override
